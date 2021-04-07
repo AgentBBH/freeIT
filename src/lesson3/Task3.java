ed240631fdc727f3be91e19312de597cf11d5e4f
Marina Smit <marina2013@tut.by>
2021-04-07 12:06:55 +0300

    Homework three (fix)

--- src/lesson3/Task3.java
+++ src/lesson3/Task3.java
@@ -7,7 +7,7 @@ public class Task3 {
     public static void main(String[] args) {
         int sum = 0;
         int count = 1;
-        while (sum < 256) {
+        while (sum <= 256) {
             sum += count;
             count *= 2;
         }
--- src/lesson3/Task4.java
+++ src/lesson3/Task4.java
@@ -13,11 +13,22 @@ public class Task4 {
         System.out.println("Input B: ");
         int b = in.nextInt();
         int result = 0;
+        boolean flagOne = true;
+        boolean flagTwo = true;
+
+        if (a < 0) {
+            flagOne = false;
+            a *= -1;
+        }
+        if (b < 0) {
+            flagTwo = false;
+            b *= -1;
+        }
 
         for (int i = 0; i < b; i++) {
             result += a;
         }
 
-        System.out.println("Result: " + result);
+        System.out.println("Result: " + ((flagOne == flagTwo) ? result : -1 * result));
     }
 }
